733e998edf61a0d09249f11f3328bbc60e75e9f0
Atharva <atharvamhaske76@gmail.com>
2025-11-22 22:38:40 +0530

    feat:few more middlewares

--- cmd/api.go
+++ cmd/api.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"net/http"
 )
 
 type application struct {
@@ -14,12 +16,23 @@ type application struct {
 func (app *application) mount() http.Handler {
 	r := chi.NewRouter()
 	
-	//middlewares
+	//good base middlewares stack
+	r.Use(middleware.RequestID) //important for rate limiting
+	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("root."))
+	})
+	
+	//set a timeout value on the request context, that will signal through
+	//ctx.Done() that the request has time out and further processing
+	//should stop
+	r.Use(middleware.Timeout(60 * time.Second))
+	
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
 	})
 	
 	// http.ListenAndServe(":8080", r)
